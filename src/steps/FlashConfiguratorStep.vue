6d85252a9505d78ab2a4f08c0ce09b92f8754436
Alex <alex@alex-dev.me>
2023-12-31 19:37:02 -0600

    Format FlashConfiguratorStep.vue file

--- src/steps/FlashConfiguratorStep.vue
+++ src/steps/FlashConfiguratorStep.vue
@@ -2,8 +2,8 @@
 import { ref, defineEmits } from 'vue'
 import { useDeviceStore } from '@/stores/devices'
 import { downloadFileWithProgress } from '@/utils/download'
-import ProgressBar from '../components/ProgressBar.vue';
-import Swal from 'sweetalert2';
+import ProgressBar from '../components/ProgressBar.vue'
+import Swal from 'sweetalert2'
 
 const emit = defineEmits(['flash'])
 
@@ -21,12 +21,11 @@ wipeToggle.value = deviceStore.flashObject.options?.cleanFlash
 verityToggle.value = deviceStore.flashObject.options?.disableVerity
 
 if (deviceStore.isABDevice) {
-  deviceStore.device.getVariable("current-slot").then((slot) => {
-    if (slot == "b") {
-      slotToggle.value = "a"
-    }
-    else {
-      slotToggle.value = "b"
+  deviceStore.device.getVariable('current-slot').then((slot) => {
+    if (slot == 'b') {
+      slotToggle.value = 'a'
+    } else {
+      slotToggle.value = 'b'
     }
   })
 }
@@ -83,9 +82,9 @@ for (const file of data.value) {
 const deleteDropFile = (filename) => {
   const index = data.value.findIndex((file) => file.filename == filename)
 
-  if (data.value[index].partition == "vbmeta") {
-    hasVbmeta.value = false;
-    verityToggle.value = false;
+  if (data.value[index].partition == 'vbmeta') {
+    hasVbmeta.value = false
+    verityToggle.value = false
   }
   data.value.splice(index, 1)
 }
@@ -110,59 +109,80 @@ const addFile = (newFiles) => {
 }
 
 const startFlash = () => {
-    deviceStore.flashObject = {
-      files: data.value,
-      options: {
-        cleanFlash: wipeToggle.value,
-        disableVerity: verityToggle.value,
-        ab: slotToggle.value
-      }
+  deviceStore.flashObject = {
+    files: data.value,
+    options: {
+      cleanFlash: wipeToggle.value,
+      disableVerity: verityToggle.value,
+      ab: slotToggle.value
     }
-    emit('flash', true)
+  }
+  emit('flash', true)
 }
 </script>
 
 <template>
-    <main>
-        <o-field id="test" :addons="false">
-              <o-table v-if="data.length > 0" :paginated="data.length > 0" :per-page="7" :striped="true" :data="data">
-                <o-table-column
-                    v-for="(column, index) in columns"
-                    :key="index"
-                    v-bind="column"
-                    #default="{ row }">
-                    <span v-if="column.field !== 'slot' && column.field !== 'partition'">{{ row[column.field] }}</span>
-                    <o-button v-if="column.field == 'slot' && deviceStore.isABDevice" @click="setFlashSlot(data.findIndex((file) => file.filename == row.filename))" color="is-danger">{{ row.slot }}</o-button>
-                    <o-input v-model="row.partition"  v-if="column.field == 'partition'"></o-input>
-                    <o-button v-if="column.field == 'deleteButton'" @click="deleteDropFile(row.filename)" color="is-danger">Delete</o-button>
-                    <ProgressBar v-if="column.field == 'size' && !row[column.field]" :progress="row.progress"/>
-                </o-table-column>
-              </o-table>
-            <o-upload :native=true @update:modelValue="addFile($event)" multiple drag-drop>
-                <section class="ex-center">
-                <p>
-                    <o-icon icon="file-upload" size="large">
-                    </o-icon>
-                </p>
-                <p>Drop your files</p>
-                </section>
-            </o-upload>
-        </o-field>
-        <o-field id="toggles">
-          <o-tooltip label="This will wipe your phone">
-            <o-switch v-model="wipeToggle">Wipe Data</o-switch>
-          </o-tooltip>
-          <o-tooltip label="Allow booting unsigned images">
-            <o-switch :disabled="!hasVbmeta" v-model="verityToggle">Disable Verity</o-switch>
-          </o-tooltip>
-          <o-tooltip label="Choose between A or B slot" v-if="deviceStore.isABDevice">
-            <o-switch v-model="slotToggle" true-value="b" false-value="a">A/B</o-switch>
-          </o-tooltip>
-        </o-field>
-        <o-field id="flash">
-            <o-button size="large" @click="startFlash()" :disabled="!(data.length > 0)">Flash</o-button>
-        </o-field>
-    </main>
+  <main>
+    <o-field id="test" :addons="false">
+      <o-table
+        v-if="data.length > 0"
+        :paginated="data.length > 0"
+        :per-page="7"
+        :striped="true"
+        :data="data"
+      >
+        <o-table-column
+          v-for="(column, index) in columns"
+          :key="index"
+          v-bind="column"
+          #default="{ row }"
+        >
+          <span v-if="column.field !== 'slot' && column.field !== 'partition'">{{
+            row[column.field]
+          }}</span>
+          <o-button
+            v-if="column.field == 'slot' && deviceStore.isABDevice"
+            @click="setFlashSlot(data.findIndex((file) => file.filename == row.filename))"
+            color="is-danger"
+            >{{ row.slot }}</o-button
+          >
+          <o-input v-model="row.partition" v-if="column.field == 'partition'"></o-input>
+          <o-button
+            v-if="column.field == 'deleteButton'"
+            @click="deleteDropFile(row.filename)"
+            color="is-danger"
+            >Delete</o-button
+          >
+          <ProgressBar
+            v-if="column.field == 'size' && !row[column.field]"
+            :progress="row.progress"
+          />
+        </o-table-column>
+      </o-table>
+      <o-upload :native="true" @update:modelValue="addFile($event)" multiple drag-drop>
+        <section class="ex-center">
+          <p>
+            <o-icon icon="file-upload" size="large"> </o-icon>
+          </p>
+          <p>Drop your files</p>
+        </section>
+      </o-upload>
+    </o-field>
+    <o-field id="toggles">
+      <o-tooltip label="This will wipe your phone">
+        <o-switch v-model="wipeToggle">Wipe Data</o-switch>
+      </o-tooltip>
+      <o-tooltip label="Allow booting unsigned images">
+        <o-switch :disabled="!hasVbmeta" v-model="verityToggle">Disable Verity</o-switch>
+      </o-tooltip>
+      <o-tooltip label="Choose between A or B slot" v-if="deviceStore.isABDevice">
+        <o-switch v-model="slotToggle" true-value="b" false-value="a">A/B</o-switch>
+      </o-tooltip>
+    </o-field>
+    <o-field id="flash">
+      <o-button size="large" @click="startFlash()" :disabled="!(data.length > 0)">Flash</o-button>
+    </o-field>
+  </main>
 </template>
 
 <style scoped>
@@ -173,6 +193,7 @@ main {
   border-radius: 25px;
   display: flex;
   justify-content: space-evenly;
+  --oruga-input-max-width: 100%;
 }
 
 .o-switch {
@@ -184,15 +205,10 @@ main {
   flex: 1;
 }
 
-main {
-  --oruga-input-max-width: 100%;
-}
-
 .o-field {
   display: flex;
   flex-direction: column;
   justify-content: center;
-
 }
 
 #test {
@@ -210,6 +226,4 @@ main {
   flex: 0 0 auto;
   margin: 0px;
 }
-
 </style>
-
