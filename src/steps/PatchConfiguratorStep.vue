ee455f6de2155cf8c3b6929744185328156daba2
Alex <alex@alex-dev.me>
2023-12-31 19:34:48 -0600

    Formatting

--- src/steps/PatchConfiguratorStep.vue
+++ src/steps/PatchConfiguratorStep.vue
@@ -64,50 +64,68 @@ const addFile = async (newFiles) => {
 }
 
 const startPatch = () => {
-    if (!data.value.length) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'You need to select at least one file!',
-        })
-        return
-    }
-    deviceStore.flashObject = {
-      files: data.value,
-    }
-    emit('patch', true)
+  if (!data.value.length) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'You need to select at least one file!'
+    })
+    return
+  }
+  deviceStore.flashObject = {
+    files: data.value
+  }
+  emit('patch', true)
 }
 </script>
 
 <template>
-    <main>
-        <o-field id="test" :addons="false">
-            <o-table v-if="data.length > 0" :paginated="data.length > 0" :per-page="7" :striped="true" :data="data">
-                <o-table-column
-                    v-for="(column, index) in columns"
-                    :key="index"
-                    v-bind="column"
-                    #default="{ row }">
-                    <span v-if="column.field !== 'slot' && column.field !== 'partition'">{{ row[column.field] }}</span>
-                    <o-button v-if="column.field == 'slot'" @click="setFlashSlot(data.findIndex((file) => file.filename == row.filename))" color="is-danger">{{ row.slot }}</o-button>
-                    <o-input v-model="row.partition"  v-if="column.field == 'partition'"></o-input>
-                    <o-button v-if="column.field == 'deleteButton'" @click="deleteDropFile(row.filename)" color="is-danger">Delete</o-button>
-                </o-table-column>
-            </o-table>
-            <o-upload :native=true @update:modelValue="addFile($event)" multiple drag-drop>
-                <section class="ex-center">
-                <p>
-                    <o-icon icon="file-upload" size="large">
-                    </o-icon>
-                </p>
-                <p>Drop your files</p>
-                </section>
-            </o-upload>
-        </o-field>
-        <o-field id="flash">
-            <o-button size="large" @click="startPatch()">Patch</o-button>
-        </o-field>
-    </main>
+  <main>
+    <o-field id="test" :addons="false">
+      <o-table
+        v-if="data.length > 0"
+        :paginated="data.length > 0"
+        :per-page="7"
+        :striped="true"
+        :data="data"
+      >
+        <o-table-column
+          v-for="(column, index) in columns"
+          :key="index"
+          v-bind="column"
+          #default="{ row }"
+        >
+          <span v-if="column.field !== 'slot' && column.field !== 'partition'">{{
+            row[column.field]
+          }}</span>
+          <o-button
+            v-if="column.field == 'slot'"
+            @click="setFlashSlot(data.findIndex((file) => file.filename == row.filename))"
+            color="is-danger"
+            >{{ row.slot }}</o-button
+          >
+          <o-input v-model="row.partition" v-if="column.field == 'partition'"></o-input>
+          <o-button
+            v-if="column.field == 'deleteButton'"
+            @click="deleteDropFile(row.filename)"
+            color="is-danger"
+            >Delete</o-button
+          >
+        </o-table-column>
+      </o-table>
+      <o-upload :native="true" @update:modelValue="addFile($event)" multiple drag-drop>
+        <section class="ex-center">
+          <p>
+            <o-icon icon="file-upload" size="large"> </o-icon>
+          </p>
+          <p>Drop your files</p>
+        </section>
+      </o-upload>
+    </o-field>
+    <o-field id="flash">
+      <o-button size="large" @click="startPatch()">Patch</o-button>
+    </o-field>
+  </main>
 </template>
 
 <style scoped>
@@ -118,17 +136,12 @@ main {
   border-radius: 25px;
   display: flex;
   justify-content: space-evenly;
-}
-
-main {
   --oruga-input-max-width: 100%;
 }
-
 .o-field {
   display: flex;
   flex-direction: column;
   justify-content: center;
-
 }
 
 #test {
@@ -147,4 +160,3 @@ main {
   margin: 0px;
 }
 </style>
-
